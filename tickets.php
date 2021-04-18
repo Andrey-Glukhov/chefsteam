a68db424e5bd7e987fe678bfd0056a384b8fd89f
Eliza <gluhova.liza@gmail.com>
2021-04-18 20:21:03 +0300

    ice-cream

--- tickets.php
+++ tickets.php
@@ -1,14 +1,21 @@
-<div class="container-fluid section_height">
+<div class="container-fluid section_height tickets_background">
     <div class="row">
-        <div class="col-sm-12"> <h2>Выбери свой формат участия:</h2></div>
-        <div class="col-md-4 col-sm-12">
-            <h2>Option1</h2>
+        <div class="col-sm-12">
+            <h2>Выбери свой формат участия:</h2>
         </div>
         <div class="col-md-4 col-sm-12">
-            <h2>Option2</h2>
+            <img class="half-width" src="http://localhost:8888/chefsteam/wordpress/wp-content/uploads/2021/04/contact_form-12-min.png"/>
         </div>
         <div class="col-md-4 col-sm-12">
-            <h2>Option3</h2>
+            <img class="half-width" src="http://localhost:8888/chefsteam/wordpress/wp-content/uploads/2021/04/contact_form-11-min.png"/>
+        </div>
+        <div class="col-md-4 col-sm-12">
+            <img class="half-width" src="http://localhost:8888/chefsteam/wordpress/wp-content/uploads/2021/04/contact_form-10-min.png"/>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-12">
+            <img class="full-width" src="http://localhost:8888/chefsteam/wordpress/wp-content/uploads/2021/04/contact_form-09-min.png"/>
         </div>
     </div>
 </div>
