9040db433475651af163f9aff31f604e3e1c550f
Andrey-Glukhov <agi2000@mail.ru>
2021-05-10 19:30:27 +0300

    change js scrollmagic

--- js/ch_team.js
+++ js/ch_team.js
@@ -1,40 +1,43 @@
-(function($) {
-var controller = new ScrollMagic.Controller();
-// slider  init
- $('.multiple-items').slick({
+(function ($) {
+ 
+  // slider  init
+  $(".multiple-items").slick({
     infinite: true,
     slidesToShow: 3,
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 2000,
-    arrows: false
+    arrows: false,
   });
 
   //<< Scroll animations
-  var scene = new ScrollMagic.Scene({
-    triggerElement: '.contact_form_wrapper',
-    duration: '100%',
-    triggerHook: 0.8
-})
-.addIndicators()
-.setTween(gsap.to('.side_pic.pic_left_2',{top: '-3vh'}))
-.addTo(controller);
-  
-scene = new ScrollMagic.Scene({
-  triggerElement: '.contact_form_wrapper',
-  duration: '100%',
-  triggerHook: 0.8
-})
-.addIndicators()
-.setTween(gsap.to('.side_pic.pic_left_1',{top: '-40vh'}))
-.addTo(controller);
-scene = new ScrollMagic.Scene({
-  triggerElement: '.contact_form_wrapper',
-  duration: '100%',
-  triggerHook: 0.8
-})
-.addIndicators()
-.setTween(gsap.to('.side_pic.pic_right_1',{top: '-37vh'}))
-.addTo(controller);
-//>>
-}(jQuery) );
+  if ($(".contact_form_wrapper").length) {
+    var controller = new ScrollMagic.Controller();
+    var scene = new ScrollMagic.Scene({
+      triggerElement: ".contact_form_wrapper",
+      duration: "100%",
+      triggerHook: 0.8,
+    })
+      .addIndicators()
+      .setTween(gsap.to(".side_pic.pic_left_2", { top: "-3vh" }))
+      .addTo(controller);
+
+    scene = new ScrollMagic.Scene({
+      triggerElement: ".contact_form_wrapper",
+      duration: "100%",
+      triggerHook: 0.8,
+    })
+      .addIndicators()
+      .setTween(gsap.to(".side_pic.pic_left_1", { top: "-40vh" }))
+      .addTo(controller);
+    scene = new ScrollMagic.Scene({
+      triggerElement: ".contact_form_wrapper",
+      duration: "100%",
+      triggerHook: 0.8,
+    })
+      .addIndicators()
+      .setTween(gsap.to(".side_pic.pic_right_1", { top: "-37vh" }))
+      .addTo(controller);
+  }
+  //>>
+})(jQuery);
